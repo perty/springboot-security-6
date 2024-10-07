7275bf08f3956690e81bb32e96d241364296e64c
perty <per@artcomputer.se>
2024-10-07 13:13:40 +0200

    Update for test running in workflow.

--- src/test/java/se/artcomputer/edu/security6/SpringSecurityUpdatedApplicationTests.java
+++ src/test/java/se/artcomputer/edu/security6/SpringSecurityUpdatedApplicationTests.java
@@ -3,7 +3,7 @@ package se.artcomputer.edu.security6;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.profiles.active=test"})
 class SpringSecurityUpdatedApplicationTests {
 
 	@Test
