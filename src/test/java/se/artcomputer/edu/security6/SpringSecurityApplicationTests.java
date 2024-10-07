5339beb3540ae4952c172e45e0cd9049dc8365d7
perty <per@artcomputer.se>
2024-10-07 14:10:29 +0200

    Rename application to SpringSecurityApplication for brevity.

--- src/test/java/se/artcomputer/edu/security6/SpringSecurityApplicationTests.java
+++ src/test/java/se/artcomputer/edu/security6/SpringSecurityApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {"spring.profiles.active=test"})
-class SpringSecurityUpdatedApplicationTests {
+class SpringSecurityApplicationTests {
 
 	@Test
 	void contextLoads() {
